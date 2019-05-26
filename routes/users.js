6257a96a1231625d5de2c803e94d938945a0306f
Sitthinon Chanaritthichai <sitthinon.c@hotmail.com>
2019-05-26 15:55:48 +0700

    update api

--- routes/users.js
+++ routes/users.js
@@ -4,18 +4,30 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
-  knex.select().from('users').then(result => {
-    res.send(result);
-  });
+  try {
+    knex.select().from('users').then(result => {
+      res.send(result);
+    });
+  } catch (err) {
+    res.send({ error: err });
+  }
 });
 
 router.post('/', (req, res, next) => {
-  knex('users').insert({
-    name: req.body.name,
-    password:req.body.password
-  }).then(resp =>{
-    res.send(resp);
-  });
+  try {
+    if (req.body.name && req.body.password) {
+      knex('users').insert({
+        name: req.body.name,
+        password: req.body.password
+      }).then(resp => {
+        res.send(resp);
+      });
+    } else {
+      res.send({ error: 'Value not valid.' });
+    }
+  } catch (err) {
+    res.send({ error: err });
+  }
 });
 
 module.exports = router;
